548ec303785ac683af1c95ae98ecce9bab753dfa
Denis Luchnikov <lasvegas3600@gmail.com>
2021-04-16 20:25:24 +0300

    Add user controller to handle post requests

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,22 +1,74 @@
+import { useState, useRef } from 'react';
+import { createUser } from '../../controllers/users';
+
 import styles from './auth-form.module.css';
 
 function AuthForm() {
+  const inputNameRef = useRef();
+  const inputEmailRef = useRef();
+  const inputPasswordRef = useRef();
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (ev) => {
+    ev.preventDefault();
+
+    const enteredName = inputNameRef.current.value;
+    const enteredEmail = inputEmailRef.current.value;
+    const enteredPassword = inputPasswordRef.current.value;
+
+    // TODO: add validation here
+    if (isLogin) {
+      //log user in
+    } else {
+      try {
+        const result = await createUser(
+          enteredName,
+          enteredEmail,
+          enteredPassword,
+        );
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <section className={styles.auth}>
-      <h1>Войти</h1>
-      <form>
+      <h1>{isLogin ? 'Вход' : 'Регистрация'}</h1>
+      <form onSubmit={submitHandler}>
+        {isLogin ? null : (
+          <div className={styles.control}>
+            <label htmlFor="name">Имя</label>
+            <input type="text" id="name" required ref={inputNameRef} />
+          </div>
+        )}
         <div className={styles.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <label htmlFor="email">Почта</label>
+          <input type="email" id="email" required ref={inputEmailRef} />
         </div>
         <div className={styles.control}>
-          <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <label htmlFor="password">Пароль</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={inputPasswordRef}
+          />
         </div>
         <div className={styles.actions}>
-          <button>Регистрация</button>
-          <button type="button" className={styles.toggle}>
-            Войти
+          <button>{isLogin ? 'Войти' : 'Создать аккаунт'}</button>
+          <button
+            type="button"
+            className={styles.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Создать аккаунт' : 'Войти'}
           </button>
         </div>
       </form>
---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+const createUser = async (name, email, password) => {
+  const response = await fetch(`/api/signup`, {
+    method: 'POST',
+    body: JSON.stringify({ name, email, password }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || 'something_went_wrong');
+  }
+  return data;
+};
+
+export { createUser };
